7736ef7b6c995c8b40fca6855ef011c4c67884a0
bobdunn <rpdunn@gmail.com>
2020-11-02 12:28:54 -0700

    add simple upload controller

---
+++ Car1/Controllers/UploadController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Car1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            using (var reader = new StreamReader(Request.Body))
+            {
+                var text = await reader.ReadToEndAsync();
+            }
+
+            return new StatusCodeResult(StatusCodes.Status202Accepted);
+        }
+    }
+}
